c81217c245bdd4e7920e610ad070e3dea0b613e0
jasdeepkaur15 <jasdeepg21@gmail.com>
2019-07-07 18:07:58 +0530

    first_commit

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+	def index
+		@profiles = Profile.all
+	end
+
+	def new
+		@profile = Profile.new
+	end
+
+	def create
+		@profile = Profile.create(permitted_params)
+		if @profile.save
+			redirect_to profiles_path
+		else
+			redirect_to @profile
+		end
+	end
+	def destroy
+		@profile = Profile.destroy(params[:id])
+		redirect_to @profile
+	end
+private
+	def permitted_params
+		params.require(:profile).permit(:name, :description)
+	end
+end
